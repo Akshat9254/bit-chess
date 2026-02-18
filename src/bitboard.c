5e9448ab821f6e348ae74b798ad0eebed75104e7
Akshat Pandey <akshatpandey660@gmail.com>
2026-02-18 22:24:50 +0530

    print bitboard

--- src/bitboard.c
+++ src/bitboard.c
@@ -4,15 +4,22 @@
 #include "board.h"
 
 void bb_print(Bitboard bb) {	
+	printf("\n");
 	for (int rank = RANK_NB - 1; rank >= 0; rank--) {
 		for (int file = 0; file < FILE_NB; file++) {
 			int sq = rank * FILE_NB + file;
 			int is_occupied = (bb & (1ULL << sq)) > 0;
-			printf("%c\t", (is_occupied ? '1': '0'));
+			if (file == 0) {
+				printf("  %d  ", rank + 1);
+			}
+			printf("%d  ", (is_occupied ? 1: 0));
 		}
 		
 		printf("\n");
 	}
+
+	printf("     a  b  c  d  e  f  g  h\n\n");
+	printf("  Bitboard = 0x%016llxULL\n\n", bb);
 }
 
 inline bool bb_test(Bitboard bb, int sq) {
